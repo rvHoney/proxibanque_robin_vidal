4ed57ab5b843cba554e73a59a17abe86590aebf3
robin.vidal <robin.vidal.pro@gmail.com>
2025-11-20 14:24:10 +0100

    core: initial commit

---
+++ src/main/java/org/formation/proxibanque_robin_vidal/ProxibanqueRobinVidalApplication.java
@@ -0,0 +1,13 @@
+package org.formation.proxibanque_robin_vidal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxibanqueRobinVidalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxibanqueRobinVidalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/formation/proxibanque_robin_vidal/ProxibanqueRobinVidalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.proxibanque_robin_vidal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxibanqueRobinVidalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
